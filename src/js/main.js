a0bdae9773a7584515570c52f2393603ff7539ec
Ian Erickson <ian.erickson@usda.gov>
2020-01-28 11:40:25 -0700

    El inicio del proyecto hacko.

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
+
+'use strict';
+
+var DocsNav = require('./components/docs-nav');
+var DocsColor = require('./components/docs-color');
+var DocsRWD = require('./components/docs-rwd');
+var NavGlobalComponent = require('./components/fsa-nav-global');
+var ModalComponent = require('./components/fsa-modal');
+var GrowlComponent = require('./components/fsa-growl');
+var SelectMultipleComponent = require('./components/fsa-select-multi');
+var SteppedControlComponent = require('./components/fsa-stepped-control');
+var SpinboxComponent = require('./components/fsa-spinbox');
+var ProgressComponent = require('./components/fsa-progress');
+var AlertComponent = require('./components/fsa-alert');
+var TableComponent = require('./components/fsa-table');
